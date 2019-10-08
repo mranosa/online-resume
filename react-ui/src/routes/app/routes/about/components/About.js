3fa44fb61bc0dd2c9b4e5b1c2ef9fce2f463fadd
Ken Ranosa <ken@signzen.ph>
2019-10-08 13:23:12 +0800

    Updated about me section

--- react-ui/src/routes/app/routes/about/components/About.js
+++ react-ui/src/routes/app/routes/about/components/About.js
@@ -4,7 +4,7 @@ import TypeScale from './TypeScale';
 import Media from './Media';
 
 const About = () => (
-  <section style={{'padding':'50px'}}>
+  <section style={{paddingTop:'50px', paddingBottom:'50px'}}>
     <article className="article" className="container-fluid col-md-10 ">
       
       <div className="box box-transparent">
@@ -23,11 +23,11 @@ const About = () => (
                   <p>I consider myself as a jack of all trade and a master of none (for now).
                   I want to experience the whole technical spectrum.</p>
                   <p>I love taking my time to enjoy the process, and constantly improve myself and my workflow along the way.</p>
-                  <p>Technically right now, my heart is leaning towards learning AI. Hopefully I get deep tech knowledge with it in the upcoming months.</p>
-                  <p>I am currently actively looking for remote work.
-                  If you think my goals, values and skills are a fit, 
+                  <p>I am currently looking for remote work. Technically, as of the moment, my heart is leaning towards AI, mobile dev or site reliability.
+                  But if you think my goals, values, skills and experience can offer value to your team, 
                   dont hesitate to send me a private message or an email. 
                   You can also reach me at <a href="https://www.linkedin.com/in/mranosa/" target="_blank">linkedin</a> or <a href="https://www.facebook.com/g3tR3ktn1gg4" target="_blank">facebook</a>.</p>
+
                 </div>
               </div>
             </div>
